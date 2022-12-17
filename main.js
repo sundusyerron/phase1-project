c913addbd8eaa197f6b95a08b290c39f8d3f7547
Sundus <sundusaden105@gmail.com>
2022-12-18 00:26:50 +0300

    Variable defination

---
+++ main.js
@@ -0,0 +1,8 @@
+// constans 
+const mealsEl = document.getElementById("meals");
+const favoriteContainer = document.getElementById("fav-meals");
+const mealPopup = document.getElementById("meal-popup");
+const mealInfoEl = document.getElementById("meal-info");
+const popupCloseBtn = document.getElementById("close-popup");
+const searchTerm = document.getElementById("search-term");
+const searchBtn = document.getElementById("search");
