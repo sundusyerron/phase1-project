0804e5416402f427e9d2595162f3d4d93c430c0c
Sundus <sundusaden105@gmail.com>
2022-12-18 00:32:42 +0300

    function implementation

--- main.js
+++ main.js
@@ -9,4 +9,36 @@ const searchBtn = document.getElementById("search");
 
 // fuction defination 
 getRandomMeal();
-fetchFavMeals();
+fetchFavMeals();`
+
+async function getRandomMeal() {
+    const resp = await fetch(
+        "https://www.themealdb.com/api/json/v1/1/random.php"
+    );
+    const respData = await resp.json();
+    const randomMeal = respData.meals[0];
+
+    addMeal(randomMeal, true);
+}
+
+async function getMealById(id) {
+    const resp = await fetch(
+        "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id
+    );
+
+    const respData = await resp.json();
+    const meal = respData.meals[0];
+
+    return meal;
+}
+
+async function getMealsBySearch(term) {
+    const resp = await fetch(
+        "https://www.themealdb.com/api/json/v1/1/search.php?s=" + term
+    );
+
+    const respData = await resp.json();
+    const meals = respData.meals;
+
+    return meals;
+}
